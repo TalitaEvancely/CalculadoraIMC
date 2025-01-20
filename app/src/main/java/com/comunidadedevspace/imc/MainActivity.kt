1f2c6c48a1da06fcdabb61af75a6677c2ebe3bf8
talita evancely <evancely@gmail.com>
2025-01-20 11:47:48 -0300

    Atualizando

--- app/src/main/java/com/comunidadedevspace/imc/MainActivity.kt
+++ app/src/main/java/com/comunidadedevspace/imc/MainActivity.kt
@@ -1,35 +1,23 @@
 package com.comunidadedevspace.imc
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.Button
-import com.google.android.material.textfield.TextInputEditText
+import androidx.appcompat.app.AppCompatActivity
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity()
+{
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        /* busca o layout com R.layout.activity_main
-         - Recurar os componentes TextInputLayout
-         - Criar uma variavel e associar(=) com o componente de UI <TextInputLayout>
-         - Recuperar o botão da tela
-         - colocar ação no botão com setOnClickListener
-         - recuperar o texto digitado no edt peso
-        */
-        val edtPeso = findViewById<TextInputEditText>(R.id.edt_weight)
+
+       /* val btnCalcular = findViewById<Button>(R.id.btn_calcular)
+        val edtPeso = findViewById<TextInputEditText>(R.id.edt_peso)
         val edtAltura = findViewById<TextInputEditText>(R.id.edt_altura)
-        val btnCalcular = findViewById<Button>(R.id.btn_calcular)
 
+        btnCalcular.setOnClickListener {
 
+            val peso:Float = edtPeso.text.toString().toFloat()
+            val altura:Float = edtAltura.text.toString().toFloat()
 
-
-
-
-      /*  btnCalcular.setOnClickListener {
-        val peso = edtPeso.text
-        println("Talita quer saber" + peso)
         }*/
-
     }
-
 }
