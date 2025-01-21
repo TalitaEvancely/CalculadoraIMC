ae41b160c8efbc6869e60bce2681d0e6c5cc6b44
talita evancely <evancely@gmail.com>
2025-01-21 14:24:13 -0300

    Mostrar a mensagem para preencher os campos

--- app/src/main/java/com/comunidadedevspace/imc/MainActivity.kt
+++ app/src/main/java/com/comunidadedevspace/imc/MainActivity.kt
@@ -3,6 +3,7 @@ package com.comunidadedevspace.imc
 import android.os.Bundle
 import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
+import com.google.android.material.snackbar.Snackbar
 import com.google.android.material.textfield.TextInputEditText
 
 class MainActivity : AppCompatActivity()
@@ -17,13 +18,25 @@ class MainActivity : AppCompatActivity()
 
         btnCalcular.setOnClickListener {
 
-            val peso:Float = edtPeso.text.toString().toFloat()
-            val altura:Float = edtAltura.text.toString().toFloat()
+            val pesoStr:String = edtPeso.text.toString()
+            val alturaStr:String = edtAltura.text.toString()
+
+            if(pesoStr == "" || alturaStr == ""){
+                //mostrar mensagem para o usuário
+                Snackbar.make(
+                    edtPeso,
+                    "Preencha os campos solicitados",
+                    Snackbar.LENGTH_LONG
+                ).show()
+            } else{
+                val peso = pesoStr.toFloat()
+                val altura = alturaStr.toFloat()
 
             val alturaQ2 = altura * altura
             val resultado = peso / alturaQ2
 
             println("Calculo IMC = " + resultado)
+        }
         }
     }
 }
